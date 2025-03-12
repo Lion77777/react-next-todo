05b2b7e7fc7b8ff8d7eea7add3199ed09ac19a31
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-03-12 14:31:49 +0400

    Add iteration functionality for list items

--- src/components/TODOList.jsx
+++ src/components/TODOList.jsx
@@ -1,7 +1,13 @@
-function TODOList({todos}) {
+import Item from "./Item";
+
+function TODOList({ todos }) {
     return (
         <ol className="todo_list">
-            
+            {todos && todos.length > 0 ? (
+                todos?.map((item, index) => <Item key={index} item={item}/>)
+            ) : (
+                <p>There is no tasks</p>
+            )}
         </ol>
     );
 }
