abab14ac8ce2fa68c5bf3413e6af1e4dbf3a27a9
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-03-12 21:20:22 +0400

    Implement delete functionalityf

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 function Item({ item, setTodos }) {
     const [editing, setEditing] = React.useState(false);
     const inputRef = React.useRef(null);
@@ -40,7 +42,11 @@ function Item({ item, setTodos }) {
 
     const handleInputBlur = () => {
         setEditing(false);
-    }
+    };
+
+    const handleDelete = () => {
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== item.id));
+    };
 
     return (
         <li className="todo_item" id={item?.id}>
@@ -94,7 +100,7 @@ function Item({ item, setTodos }) {
                                 />
                             </svg>
                         </button>
-                        <button>
+                        <button onClick={handleDelete}>
                             <span className="visually-hidden">Delete</span>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
